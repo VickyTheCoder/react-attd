d568cac43718f88ca05028bcf4ab916bc35bc920
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2025-02-07 08:11:29 +0530

    fixed

--- src/components/Active.tsx
+++ src/components/Active.tsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-function Active(){
-  const [isSelected, setIsSelected] = useState(false);
+const Active: React.FC = () => {
+  // Ensure it starts as deselected
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+
   const OnOff = () => {
     setIsSelected((prev) => !prev);
-  }
-    return (
-        <div>
-          <h2>Toggle Control</h2>
+  };
+
+  return (
+    <div>
+      <h2>Toggle Control</h2>
       <button onClick={OnOff}>
         {isSelected ? "In ✅" : "Out ❌"}
       </button>
-      <p>Option is {isSelected ? "Selected" : "Deselected "}</p>
-          <a href="/">Go Back</a>
-        </div>
-      );
-}
+      <p>Option is {isSelected ? "Selected" : "Deselected"}</p>
+      <a href="/">Go Back</a>
+    </div>
+  );
+};
 
 export default Active;
